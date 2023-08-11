304ef09314b874830026348c21f2d6ba4ae80304
maruf2023 <maruf1916810369@gmailcom>
2023-08-11 18:00:13 +0600

    added the portfolio. html

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ window.addEventListener ("scroll", function(){
   header.classList.toggle ("sticky", window.scrollY> 120);
 })
 
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist')
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x')
+  navlist.classList.toggle('active')
+};
+
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x')
+  navlist.classList.remove('active')
+}
