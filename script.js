bee2306f364ece7700a0aa2f0b5ec554e6d73848
maruf2023 <maruf1916810369@gmailcom>
2023-08-12 09:17:22 +0600

    compelet the full project

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ window.addEventListener ("scroll", function(){
   header.classList.toggle ("sticky", window.scrollY> 120);
 })
 
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist')
 
-menu.onclick = () => {
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navlist');
+
+menu.onclick = () =>{
   menu.classList.toggle('bx-x')
   navlist.classList.toggle('active')
-};
+}
 
-
-window.onscroll = () => {
+window.onscroll = () =>{
   menu.classList.remove('bx-x')
   navlist.classList.remove('active')
 }
